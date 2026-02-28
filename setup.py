ea40f62e8b62a0907f109760bd321eac3cf71e63
Neo Yi Ting <t.ingyyy05@gmail.com>
2026-02-28 14:14:29 +0800

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Chatbot with RAG",
+    version="0.1.0",
+    author="Neo Yi Ting",
+    author_email="t.ingyyy05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
